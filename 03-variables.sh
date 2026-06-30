b0930cf9cb73c27af331a781ac1fdd3ff1bfb0b6
pavan <pavankumar49210@gmail.com>
2026-06-30 19:19:39 +0530

    addded

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: I'm fine $PERSON1"
+echo "$PERSON2: that's good $PERSON1"
+echo "$PERSON1: thanks $PERSON2"
