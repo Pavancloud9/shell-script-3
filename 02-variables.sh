7004b2e1ef57cd05f1a126804a755cc60fce7904
pavan <pavankumar49210@gmail.com>
2026-06-30 19:09:25 +0530

    added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "rahim: how are you robert"
-echo "robert: I'm fine rahim"
-echo "rahim: that's good robert"
-echo "robert: thanks rahim"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: I'm fine $PERSON1"
+echo "$PERSON2: that's good $PERSON1"
+echo "$PERSON1: thanks $PERSON2"
