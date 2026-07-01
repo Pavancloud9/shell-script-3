4427dd1a6f99bb212907221e33a9ae3dba4a1ccd
pavan <pavankumar49210@gmail.com>
2026-07-01 20:09:16 +0530

    added

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -4,6 +4,8 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)
+echo "Script executed at $TIMESTAMP"
+
 SUM=$(($NUMBER1+$NUMBER2))
 
-## echo "The addition of $NUMBER1 and $NUMBER2 is $SUM" 
+echo "The addition of $NUMBER1 and $NUMBER2 is $SUM" 
