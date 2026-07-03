a02e16a45db8d15c5445a4b421426d7cad3c39df
pavan <pavankumar49210@gmail.com>
2026-07-03 16:12:08 +0530

    added

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,11 +4,24 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "You must have SUDO Access to perform this action"
+    echo "You Must have SUDO Access to perform this action"
     exit 1
 fi
 
 dnf install mysql -y
 if [ $? -ne 0 ]
 then
+    echo "Installing MYSQL...FAILURE"
+    exit 1
+else
+    echo "Installign MYSQL...SUCCESS"
+fi
 
+dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "Installing GIT...FAILURE"
+    exit 1
+else
+    echo "Installing GIT...SUCCESS"
+fi
