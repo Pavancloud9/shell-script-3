ed04de8ad4883a5b6c7caa38bc504bdce465dfcf
pavan <pavankumar49210@gmail.com>
2026-07-06 21:48:43 +0530

    added

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -2,7 +2,7 @@
 
 SOURCE_DIR="/home/ec2-user/app-logs"
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.logs" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 echo "Files to be deleted:: $FILES_TO_DELETE"
 
