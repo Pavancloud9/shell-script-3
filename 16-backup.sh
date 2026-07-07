810b4200b8e1305ef32dfd7cfa534d1786c3c61e
pavan <pavankumar49210@gmail.com>
2026-07-07 20:30:48 +0530

    addd

---
+++ 16-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)    ### 13-loops
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+echo "Script started exectuing at: $TIMESTAMP" &>>LOG_FILE_NAME
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # If user is not providing number of days, we are taking 14 as default
+
+USAGE(){
+    echo "USAGE:: sh 16-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>(optional)"
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
