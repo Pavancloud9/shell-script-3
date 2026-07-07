6a5bba20f887837fec16b606b15a0b61938ee857
pavan <pavankumar49210@gmail.com>
2026-07-07 15:27:14 +0530

    addd

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -32,7 +32,7 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted:: $FILES_TO_DELETE"
 
 
-while read -r file
+while read -r line
 do
-    echo "Deleting file: $file"
+    echo "Deleting file: $line"
 done <<< $FILES_TO_DELETE
