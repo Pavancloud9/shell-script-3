0185f9b9fef2c4cc60f328c7db59c49fcc90d08a
pavan <pavankumar49210@gmail.com>
2026-07-11 16:32:35 +0530

    added

--- 17-backup.sh
+++ 17-backup.sh
@@ -39,7 +39,7 @@ if [ -n "$FILES" ]
 then
     echo "Files are:: $FILES"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime $DAYS | zip $ZIP_FILE -@
+    find $SOURCE_DIR -name "*.log" -mtime $DAYS | zip -@ "$ZIP_FILE" 
     if [ -f $ZIP_FILE  ]
     then
         echo "Failed to create ZIP FILE"
