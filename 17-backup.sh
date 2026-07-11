c1e3364445903202c6f879c35b1ddecbc71fa506
pavan <pavankumar49210@gmail.com>
2026-07-11 14:59:39 +0530

    added

--- 17-backup.sh
+++ 17-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/shell-script-logs"
+LOGS_FOLDER="/home/ec2-user/shell-script-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
